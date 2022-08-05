795ca0c9df7d3222ea8678958cd67ba2dbabf0d2
Abdelhakim <elouakouakabdelhakim@gmail.com>
2022-08-05 13:48:58 +0100

    completed:
    * the project is now considered done as it meets the requirement set
    earlier
    * the project has a default mode and a customized one

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
 const container = document.querySelector(".container");
+const dimButton = document.querySelector(".dim");
+const clearButton = document.querySelector(".clr");
 
-//this function should be modify later
-function makeRows(rows, cols) {
+dimButton.addEventListener("click", function(){
+    const dim = Number(prompt("enter your dimension (100 max)"));
+    makeGrid(dim, dim);
+    addhover();
+});
+
+clearButton.addEventListener("click", function(){
+    document.querySelectorAll(".item").forEach(cell => cell.style.backgroundColor="White");
+})
+
+function makeGrid(rows, cols) {
+    container.textContent = "";
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "item";
+
+      cell.style.width = `${512/cols}px`;
+      cell.style.height = `${512/rows}px`;
     };
 };
 
-makeRows(16, 16)
-
-const cells = document.querySelectorAll(".item");
-
-cells.forEach(cell => {
+function addhover(){
+    const cells = document.querySelectorAll(".item");
+    cells.forEach(cell => {
     cell.addEventListener("mouseover", function(){
-        cell.style.backgroundColor = "black";
+    cell.style.backgroundColor = "black";
     });
 });
+}
+
+function dft(){ //the model to display by default
+    makeGrid(16,16);
+    addhover();
+}
+
+dft();
